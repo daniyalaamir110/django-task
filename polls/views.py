443f0d35b861adc46bf51aa099668088f9a4b7e8
daniyalaamir110 <daniyal.amir110@gmail.com>
2023-09-07 16:53:52 +0500

    generic views

--- polls/views.py
+++ polls/views.py
@@ -1,23 +1,26 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
+from django.views import generic
 from .models import *
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {
-        "latest_question_list": latest_question_list
-    }
-    return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {"question": question}
-    return render(request, "polls/detail.html", context)
+    template_name = "polls/index.html"
+    context_object_name = "lastest_question_list"
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+
+class DetailView(generic.DetailView):
+
+    template_name = "polls/detail.html"
+    model = Question
+
+class ResultsView(generic.DetailView):
+
+    template_name = "polls/results.html"
+    model = Question
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
